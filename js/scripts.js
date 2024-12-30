deb390df68808d501ff02d4bbac8571a04cf40c3
Jake Fifer <jakefifer@gmail.com>
2024-12-29 20:17:30 -0500

    removed js method for navbar

--- js/scripts.js
+++ js/scripts.js
@@ -21,26 +21,26 @@
 	});
 
 	
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 60) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
-    });
+	// /* Navbar Scripts */
+	// // jQuery to collapse the navbar on scroll
+    // $(window).on('scroll load', function() {
+	// 	if ($(".navbar").offset().top > 60) {
+	// 		$(".fixed-top").addClass("top-nav-collapse");
+	// 	} else {
+	// 		$(".fixed-top").removeClass("top-nav-collapse");
+	// 	}
+    // });
 
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+	// // jQuery for page scrolling feature - requires jQuery Easing plugin
+	// $(function() {
+	// 	$(document).on('click', 'a.page-scroll', function(event) {
+	// 		var $anchor = $(this);
+	// 		$('html, body').stop().animate({
+	// 			scrollTop: $($anchor.attr('href')).offset().top
+	// 		}, 600, 'easeInOutExpo');
+	// 		event.preventDefault();
+	// 	});
+	// });
 
     // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function(event) {
